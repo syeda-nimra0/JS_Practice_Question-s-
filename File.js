861185b983fda93a268f6a5291cea3c3f41ea8f7
syeda-nimra0 <nimrasyeda37@gmail.com>
2025-05-25 22:53:37 +0500

    Add files via upload

---
+++ File.js
@@ -0,0 +1,80 @@
+   // CHAPTER: 01:
+  // QUESTION # 01:
+ // ALERT BOX;  
+   alert("Hello! Welcome to our Website! We hope you have a great time here.");
+    
+   // QUESTION # 02:
+   // ALERT BOX;
+    alert("Error! Please enter a valid password.")
+
+    // QUESTION # 03:
+    // DISPLAY MESSAGE WITH LINE BREAK IN ALERT BOX;
+    alert("Welcome to JS Land\nHappy Coding!")
+
+    // QUESTION # 04:
+    //MESSAGE 01:
+    alert("Welcome to JS Land...")
+    // MESSAGE 02:
+    alert("Happy Coding!\n🔘Prevent this page from creating additional dialogs.")
+
+    // QUESTION # 05:
+    // MESSAGE IN DEVELOPER CONSOLE;
+    console.log("Hello...I can run JS through my web browser's console.")
+
+    // QUESTION # 06:
+    // USE ALERT BOX IN YOUR PROJECT;
+
+    alert("Hello!please click this button.");
+    
+
+   // CHAPTER: 02:
+   // QUESTION # 01:
+   //DECLARE VARIABLE USERNAME;
+   let Username;
+   // QUESTION # 02:
+   let username = "Nimra Basharat";
+    console.log(username);
+    // QUESTION # 03:
+    let message = "Hello World!";
+    alert(message);
+   // QUESTION # 04:
+    var studentName = "Nimra";
+    var studentAge = 19;
+    var studentGender = "Female";
+    var studentCourse = "Mathematics";
+    var studentUniversity = "XYZ University";
+   alert("Student Bio Data:\n" +  "Name:" + studentName + "\n"  + "Age:" + studentAge + "\n" + "Gender:" + studentGender + "\n" + "Course:" + studentCourse + "\n" + "University:" + studentUniversity);
+  
+   //showing in seperate alert boxes; 
+alert("Age: " + studentAge);
+alert("Gender: " + studentGender);
+alert("Course: " + studentCourse);
+alert("University: " + studentUniversity);
+
+
+
+  // QUESTION # 05:
+  //Question 5: Display this alert using one JS variable;
+  var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+   alert(pizza);
+   //Question 6: Show Email using alert (string concatenation);
+   var email = "123@gmail.com";
+   alert("My email address is" + email);
+   //Question 7: Show book name in an alert box;
+   var book = "A Smarter way to learn JavaScript.";
+   alert("I'm trying to learn from the book" + book);
+   //Question 8: Show this text in browser using JS;
+   document.write("Yah! I can write HTML content through JavaScript");
+   //Question 9: Store string in variable and show in alert + browser;
+   var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+   alert(design);
+document.write(design);
+
+//end---
+
+
+
+
+
+
+                
